7955e4ccab5bb94917d62aaf74a72ae6b9319c2c
Lu1zOtav1o <otaviomatoss7@gmail.com>
2024-10-30 08:41:19 -0300

    Adicionado botão Cadastros, pagina de cadastro e style

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <h1>Geovanna Chiara e Luiz Otávio</h1>
+    <a href="cadastros.php">
+        <button>Cadastros</button>
+    </a>
     <img src="Imagens/pizza.webp" />
 </body>
 </html>
